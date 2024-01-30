8fac6775927252de54304bd2a440f6f2edadef2f
LutekDev <lutekdev@gmail.com>
2024-01-30 09:52:04 -0300

    :tada: Update New Files of Project Actual

--- js.AULAS/aula.004/index.js
+++ js.AULAS/aula.004/index.js
@@ -1,7 +1,10 @@
-let nome = "Lutek";
+let nomeHomem, nomeMulher, nomeFilho;
+nomeHomem = "LutekDev";
+nomeMulher = "Princesa";
+nomeFilho = "Pequeno";
 
-console.log(nome, "Nasceu em 1984.");
-console.log("Em 2000", nome, "se conheceu Maria.");
-console.log(nome, "casou-se com Maria em 2012.");
-console.log("Maria teve 1 filho com", nome, "em 2015.");
-console.log("O Filho de", nome, "se chama Eduardo.");
+console.log(nomeHomem, "Nasceu em 1984.");
+console.log("Em 2000", nomeHomem, "conheceu", nomeMulher, ".");
+console.log(nomeHomem, "casou-se com", nomeMulher, "em 2012.");
+console.log(nomeMulher, "teve 1 filho com", nomeHomem, "em 2015.");
+console.log("O Filho de", nomeHomem, "se chama", nomeFilho, ".");
