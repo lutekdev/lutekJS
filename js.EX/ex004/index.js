83de81fe9ea4e8b4ead4d54e9584f9e838dafdc2
LutekDev <lutekdev@gmail.com>
2024-01-30 11:43:03 -0300

    :tada: Update New Files of Project Actual

--- js.EX/ex004/index.js
+++ js.EX/ex004/index.js
@@ -4,12 +4,14 @@ let varA = "A"; // B
 let varB = "B"; // C
 let varC = "C"; // A
 
-// console.log(varA, varB, varC);
-
-let temp = varA;
-
-varA = varB;
-varB = varC;
-varC = temp;
+[varA, varB, varC] = [varB, varC, varA];
 
 console.log(varA, varB, varC);
+
+// let temp = varA;
+
+// varA = varB;
+// varB = varC;
+// varC = temp;
+
+// console.log(varA, varB, varC);
