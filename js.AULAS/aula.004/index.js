14e4c97e7ab32c4f4d1458c363e93f43484a1e07
LutekDev <lutekdev@gmail.com>
2024-01-30 10:14:45 -0300

    :tada: Update New Files of Project Actual

--- js.AULAS/aula.004/index.js
+++ js.AULAS/aula.004/index.js
@@ -3,8 +3,20 @@ nomeHomem = "LutekDev";
 nomeMulher = "Princesa";
 nomeFilho = "Pequeno";
 
+console.log("=====================================");
+
 console.log(nomeHomem, "Nasceu em 1984.");
 console.log("Em 2000", nomeHomem, "conheceu", nomeMulher, ".");
 console.log(nomeHomem, "casou-se com", nomeMulher, "em 2012.");
 console.log(nomeMulher, "teve 1 filho com", nomeHomem, "em 2015.");
 console.log("O Filho de", nomeHomem, "se chama", nomeFilho, ".");
+
+console.log("=====================================");
+
+var nome;
+nome = "Teste";
+console.log(nome);
+var nome = "Ola";
+console.log(nome);
+
+console.log("=====================================");
--- js.AULAS/aula.005/index.js
+++ js.AULAS/aula.005/index.js
@@ -7,12 +7,3 @@ console.log("Em 2000", nomeHomem, "conheceu", nomeMulher, ".");
 console.log(nomeHomem, "casou-se com", nomeMulher, "em 2012.");
 console.log(nomeMulher, "teve 1 filho com", nomeHomem, "em 2015.");
 console.log("O Filho de", nomeHomem, "se chama", nomeFilho, ".");
-
-
-
-var nome
-nome = "Teste"
-
-var nome = "Ola"
-
-console.log(nome)
