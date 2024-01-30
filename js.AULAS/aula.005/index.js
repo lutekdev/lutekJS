c8c331c43383fbd4cad5acc855d86c7bd23e60e7
LutekDev <lutekdev@gmail.com>
2024-01-30 10:05:22 -0300

    :tada: Update New Files of Project Actual

--- js.AULAS/aula.005/index.js
+++ js.AULAS/aula.005/index.js
@@ -7,3 +7,12 @@ console.log("Em 2000", nomeHomem, "conheceu", nomeMulher, ".");
 console.log(nomeHomem, "casou-se com", nomeMulher, "em 2012.");
 console.log(nomeMulher, "teve 1 filho com", nomeHomem, "em 2015.");
 console.log("O Filho de", nomeHomem, "se chama", nomeFilho, ".");
+
+
+
+var nome
+nome = "Teste"
+
+var nome = "Ola"
+
+console.log(nome)
