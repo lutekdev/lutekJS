612032db808f41900d1244dd88d5e50bf4b8d992
LutekDev <lutekdev@gmail.com>
2024-02-08 12:11:56 -0300

    :tada: Update New Files and Update README.md

--- js.AULAS/aula.014/index.js
+++ js.AULAS/aula.014/index.js
@@ -1,16 +1,28 @@
-function saudacao(nome) {
-    // console.log(`Bom Dia '${nome}'`);
+// function saudacao(nome) {
+//     // console.log(`Bom Dia '${nome}'`);
+//     return `Bom Dia '${nome}'`;
+// }
+//
+// function soma(num1, num2) {
+//     return num1 + num2;
+// }
+//
+// // ---------------------- //
+// const variavel = saudacao("LutekDev");
+// // ---------------------- //
+// console.log(variavel);
+// // ---------------------- //
+// console.log(soma(5, 10));
+// // ---------------------- //
+
+// Criando função anonima
+const variavel2 = function (nome) {
     return `Bom Dia '${nome}'`;
-}
+};
 
-function soma(num1, num2) {
-    return num1 + num2;
-}
+const raiz = function (n) {
+    return n ** 0.5;
+};
 
-// ---------------------- //
-const variavel = saudacao("LutekDev");
-// ---------------------- //
-console.log(variavel);
-// ---------------------- //
-console.log(soma(5, 10));
-// ---------------------- //
+console.log(variavel2("LutekDev"));
+console.log(raiz(9));
