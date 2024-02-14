4cdd59abb98aebae1c18168ef6bc3492ccc763bc
LutekDev <lutekdev@gmail.com>
2024-02-14 19:48:09 -0300

    📦 NEW: Added Ex018 in 14/02/24

--- js.AULAS/aula.018/index.js
+++ js.AULAS/aula.018/index.js
@@ -13,3 +13,12 @@ const expressaoNot = !true;
 console.log(expressaoAnd);
 console.log(expressaoOr);
 console.log(expressaoNot);
+
+const member = "LutekDev";
+const password = "123456";
+
+const isLogged = member === "LutekDev" && password === "123456";
+console.log(isLogged)
+
+const isNotLogged = !isLogged;
+console.log(isNotLogged);
