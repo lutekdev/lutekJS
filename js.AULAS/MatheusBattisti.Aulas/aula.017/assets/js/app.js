d2534e23c7e56774ff724650df6354f56cc79837
LutekDev <lutekdev@gmail.com>
2024-03-04 15:24:33 -0300

    📦 NEW: Added New Files in 04/03/24

--- js.AULAS/MatheusBattisti.Aulas/aula.017/assets/js/app.js
+++ js.AULAS/MatheusBattisti.Aulas/aula.017/assets/js/app.js
@@ -6,4 +6,5 @@ let maiorNumero = Math.max(1, 5, 2, 3);
 let menorNumero = Math.min(1, 5, 2, 3);
 alert(`O maior número é ${maiorNumero} e o menor número é ${menorNumero}`);
 
-let numberRamd = Math.random() * 10;
+let numberRandom = Math.random() * 10;
+alert(`O número aleatório é ${numberRandom.toFixed(2)}`);
