5735574b56e02556d495f0d74eb33f9111f1b6b3
LutekDev <lutekdev@gmail.com>
2024-03-04 15:23:59 -0300

    📦 NEW: Added New Files in 04/03/24

--- js.AULAS/MatheusBattisti.Aulas/aula.017/assets/js/app.js
+++ js.AULAS/MatheusBattisti.Aulas/aula.017/assets/js/app.js
@@ -1,3 +1,9 @@
 let nome = prompt("Digite seu Nome:");
 let idade = prompt("Digite sua Idade:");
 alert(`Hello, World ${nome}, você tem ${idade} anos`);
+
+let maiorNumero = Math.max(1, 5, 2, 3);
+let menorNumero = Math.min(1, 5, 2, 3);
+alert(`O maior número é ${maiorNumero} e o menor número é ${menorNumero}`);
+
+let numberRamd = Math.random() * 10;
